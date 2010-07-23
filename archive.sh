e1628409a5f6cdb48e8ea411f4e1bc2aed8bca94
Vincent Driessen <vincent@datafox.nl>
2010-07-23 15:03:54 +0200

    Add archiver, to quickly archive the whole .vim directory.
    This archive will include all subprojects and will not contain any .git
    files.

---
+++ archive.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd $(dirname $0)
+cd ..
+tar -czvf vimrc.tgz --exclude='vimrc/vim/view' --exclude='.git' --exclude='*.DS_Store' --exclude='vimrc/vim/user.vim*' --exclude='vim/NERDTreeBookmarks' vimrc/*
