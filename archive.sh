bbffbb5ef7e1069b4032404dcf7c14d9b539ecb6
Vincent Driessen <vincent@datafox.nl>
2010-07-24 14:23:18 +0200

    Fix long line in archive.sh.

--- archive.sh
+++ archive.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 cd $(dirname $0)
 cd ..
-tar -czvf vimrc.tgz --exclude='vimrc/vim/view' --exclude='.git' --exclude='*.DS_Store' --exclude='vimrc/vim/user.vim*' --exclude='vim/NERDTreeBookmarks' vimrc/*
+tar -czvf vimrc.tgz \
+    --exclude='vimrc/vim/view' \
+    --exclude='.git' \
+    --exclude='*.DS_Store' \
+    --exclude='vimrc/vim/user.vim*' \
+    --exclude='vim/NERDTreeBookmarks' \
+    vimrc/*
