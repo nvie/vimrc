feeee6d3a0af6f640ad32c65ef477a7764dba61b
Vincent Driessen <vincent@datafox.nl>
2010-08-04 11:03:30 +0200

    Put pyunit submodule on develop tip, instead of master.

--- vim/bundle/check_for_updates.sh
+++ vim/bundle/check_for_updates.sh
@@ -6,7 +6,11 @@ for submodule in *; do
 	if [ -d "$submodule/.git" ]; then
 		cd "$submodule"
 		echo "--- $submodule:"
-		git co master
+		if [ "$submodule" = "pyunit" ]; then
+			git co develop
+		else
+			git co master
+		fi
 		git pull
 		cd ..
 	fi
