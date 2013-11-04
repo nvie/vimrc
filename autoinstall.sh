e87e1b351ecd49b87abd787a26f3a77f477da906
Vincent Driessen <vincent@3rdcloud.com>
2013-11-04 11:31:30 +0100

    Change auto-installation.

--- autoinstall.sh
+++ autoinstall.sh
@@ -10,26 +10,26 @@ die() {
     exit 1
 }
 
-[ -e "$INSTALL_TO/vimrc" ] && die "$INSTALL_TO/vimrc already exists."
-[ -e "~/.vim" ] && die "~/.vim already exists."
-[ -e "~/.vimrc" ] && die "~/.vimrc already exists."
+install_vimrc () {
+    [ -e "$INSTALL_TO/vimrc" ] && die "$INSTALL_TO/vimrc already exists."
+    [ -e "~/.vim" ] && die "~/.vim already exists."
+    [ -e "~/.vimrc" ] && die "~/.vimrc already exists."
 
-cd "$INSTALL_TO"
-git clone git://github.com/nvie/vimrc.git
-cd vimrc
+    cd "$INSTALL_TO"
+    git clone git://github.com/nvie/vimrc.git
+    cd vimrc
 
-# Download vim plugin bundles
-git submodule init
-git submodule update
+    # Download vim plugin bundles
+    git submodule init
+    git submodule update
 
-# Compile command-t for the current platform
-cd vim/ruby/command-t
-(ruby extconf.rb && make clean && make) || warn "Ruby compilation failed. Ruby not installed, maybe?"
+    # Symlink ~/.vim and ~/.vimrc
+    cd ~
+    ln -s "$INSTALL_TO/vimrc/vimrc" .vimrc
+    ln -s "$INSTALL_TO/vimrc/vim" .vim
+    touch ~/.vim/user.vim
 
-# Symlink ~/.vim and ~/.vimrc
-cd ~
-ln -s "$INSTALL_TO/vimrc/vimrc" .vimrc
-ln -s "$INSTALL_TO/vimrc/vim" .vim
-touch ~/.vim/user.vim
+    echo "Installed and configured .vim, have fun."
+}
 
-echo "Installed and configured .vim, have fun."
+install_vimrc
