93eeec137df7da634a615e2cb436e53a4dce4190
Vincent Driessen <me@nvie.com>
2021-07-05 12:43:28 +0200

    Remove custom scripts

--- add-bundle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ $# -lt 1 ]; then
-    echo "Bundle URL? \c"
-    read url
-else
-    url="$1"
-fi
-git submodule add --force "$url" vim/pack/nvie/start/$(basename "$url" | sed 's/\.git$//')
